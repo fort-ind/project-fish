d0ce567f6d81a3d26fdf206fddc656953f65eead
Ranvir Singh <soomaln02@gmail.com>
2026-08-10 21:10:20 -0500

    the start?

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace fortindwindows
         {
             InitializeComponent();
         }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
